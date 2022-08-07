a0d52f686189de52bede119585604c5a1273dc0d
Jhonny Sánchez <jhonny.sanchez@koahealth.com>
2022-08-07 14:27:27 +0200

    update some literals

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { CMS_NAME } from '../lib/constants'
 import { indexQuery } from '../lib/queries'
 import { getClient, overlayDrafts } from '../lib/sanity.server'
 
@@ -15,7 +14,7 @@ export default function Index({ allPosts, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Alba Cirera portfolio</title>
         </Head>
         <Container>
           <Intro />
