cf2a30ed198291e10aa9184e6ad2b521c4332f28
Jhonny Sánchez <developer@jhonnysanchez.dev>
2022-08-07 20:09:16 +0000

    update contact styles

--- components/contact.js
+++ components/contact.js
@@ -1,13 +1,13 @@
 export default function Contact({ contact, resume, email }) {
     return (
-        <div className="flex flex-col md:flex-row justify-end md:ml-10 mt-8 md:mb-12 space-x-4  md:space-y-0 space-y-2">
-            <a href="/alba-cv.pdf" target="_blank" className="text-slate-700 hover:bg-slate-200 font-bold py-2 px-4 underline inline-flex items-center rounded">
+        <div className="flex flex-col md:flex-row justify-end md:ml-10 mt-8 md:mb-12">
+            <a href="/alba-cv.pdf" target="_blank" className="w-36 justify-center text-slate-700 hover:bg-slate-200 font-bold py-2 px-4 underline inline-flex items-center rounded mr-2 mb-2 md:mb-0">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
                 {resume ?? "Veure CV"}
             </a>
-            <a href={`mailto:${email ?? "albacirerasant@gmail.com"}`} className="bg-slate-300 hover:bg-slate-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <a href={`mailto:${email ?? "albacirerasant@gmail.com"}`} className="w-36 justify-center bg-slate-300 hover:bg-slate-400 text-slate-800 font-bold py-2 px-4 rounded inline-flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
--- components/more-stories.js
+++ components/more-stories.js
@@ -7,7 +7,7 @@ export default function MoreStories({ title, stories }) {
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
         {title}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-12 gap-y-12 mb-32">
         {stories?.map((story) => (
           <PostPreview
             key={story.slug}
