6819ccfc0d22afc4d631b7caaafafd887dd30709
Jhonny Sánchez <developer@jhonnysanchez.dev>
2022-08-07 14:08:30 +0000

    update project url for preview

--- studio/resolveProductionUrl.js
+++ studio/resolveProductionUrl.js
@@ -1,5 +1,5 @@
 const previewSecret = 'MY_SECRET' // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000'
+const projectUrl = 'https://albacirera.com'
 
 export default function resolveProductionUrl(document) {
   return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
