28cd7f9cde3f9919d978d1a7f04637a565a55d7f
Jhonny Sánchez <developer@jhonnysanchez.dev>
2022-11-08 22:08:04 +0000

    link to resume and make buttons bigger

--- components/intro.js
+++ components/intro.js
@@ -6,18 +6,18 @@ export default function Intro({ title, subtitle, mainImage, contact, resume, ema
     <section >
       <div className="grid grid-cols-1 md:grid-cols-2  items-center md:justify-between mt-16 mb-16 md:mb-12">
         <div>
-          <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          <h1 className="text-3xl md:text-6xl font-bold tracking-tighter leading-tight md:pr-8">
             {title}
           </h1>
-          <h4 className="text-left text-lg mt-5">
+          <h4 className="text-left text-xl mt-5">
             {subtitle}
           </h4>
         </div>
         <Contact contact={contact} email={email} resume={resume}/>
       </div>
-      <div className="mb-8 md:mb-16">
+      {/* <div className="mb-8 md:mb-16">
         <CoverImage title={title} image={mainImage} />
-      </div>
+      </div> */}
     </section>
   );
 }
